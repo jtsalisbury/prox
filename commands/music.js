b34c2fe715e3a2cca6ee36ebc9f7275263fd218f
crazyscouter <crazyscouter1@gmail.com>
2020-01-01 17:52:10 -0500

    Adds playlist support for music

--- commands/music.js
+++ commands/music.js
@@ -1,12 +1,17 @@
 const ytdl = require('ytdl-core');
+const ytlist = require('youtube-playlist');
+
+// Master storage for all queues across servers
 let serverQueues = new Map();
 
 let verifyChannelPerms = function(message) {
+    // Verify that the user is in a voice channel
     let voiceCh = message.member.voiceChannel;
     if (!voiceCh) {
         global.cbot.sendError('You need to be in a voice channel to play music');
     }
 
+    // Check that we have the correct permissions
     let perms = voiceCh.permissionsFor(message.client.user);
     if (!perms.has('CONNECT') || !perms.has('SPEAK')) {
         global.cbot.sendError('I need permission to join and speak here!');
@@ -25,6 +30,7 @@ let getServerQueue = function(guildId) {
 let verifyChannelMembers = function(guildId) {
     let queue = getServerQueue(guildId);
 
+    // If the only member is one, that means it's the bot, so leave the channel
     if (queue.connection.channel.members.length === 1) {
         queue.connection.channel.leave();
         serverQueues.delete(guildId);
@@ -40,12 +46,13 @@ let playNextSong = function(guildId, channel) {
         queue.connection.channel.leave();
         serverQueues.delete(guildId);
 
-        return;
+        global.cbot.sendError('No songs left in the queue, leaving');
     }
 
     verifyChannelMembers(guildId);
     let curSong = queue.songs[0];
 
+    // Create a new download stream from YouTube
     let stream = ytdl(curSong.url, {
         filter: 'audioonly'
     });
@@ -53,8 +60,9 @@ let playNextSong = function(guildId, channel) {
         global.cbot.sendError(err);
     });
 
+    // Create a new dispatcher to play our stream
     let dispatcher = queue.connection.playStream(stream);
-    dispatcher.on('end', (reason) => {
+    dispatcher.on('end', () => {
         queue.songs.shift();
 
         playNextSong(guildId, channel);
@@ -63,6 +71,7 @@ let playNextSong = function(guildId, channel) {
         global.cbot.sendError(err);
     });
 
+    // Print the next song
     let nextSong = '';
     if (queue.songs.length > 1) {
         nextSong = `The next song is **${queue.songs[1].title}**`;
@@ -70,18 +79,19 @@ let playNextSong = function(guildId, channel) {
         nextSong = 'There is no song up next';
     }
 
+    // Set the volume logarithmically
     dispatcher.setVolumeLogarithmic(queue.volume / 5);
 
     global.cbot.sendMessage(`Now playing **${curSong.title}**\n${nextSong}`, channel);
 }
 
-let createNewQueue = async function(message, song) {
+let createNewQueue = async function(message, songs) {
     // Create a new queue object and add the song
     let queue = {
         textChannel: message.channel,
         voiceChannel: message.member.voiceChannel,
         connection: null,
-        songs: [song],
+        songs: songs,
         volume: 5,
         playing: true
     };
@@ -92,6 +102,8 @@ let createNewQueue = async function(message, song) {
 
     // Add the guild queue to the map
     serverQueues.set(message.guild.id, queue);
+
+    return queue;
 }
 
 
@@ -107,28 +119,46 @@ play.params = [
 ];
 play.callback = async function(message, link) {
     verifyChannelPerms(message);
-
-    let songInfo = await ytdl.getInfo(link);
-    let song = {
-        title: songInfo.title,
-        url: songInfo.video_url
+    
+    // Determine whether we are dealing with a playlist or not
+    let songs = [];
+    if (link.indexOf('&list=') !== -1) {
+        // Query YouTube to get all the songs in the playlist
+        let result = await ytlist(link, ['name', 'url']);
+        result.data.playlist.forEach(obj => {
+            songs.push({
+                title: obj.name,
+                url: obj.url
+            });
+        })
+    } else {
+        // Grab the info for our one song we want to add
+        let songInfo = await ytdl.getInfo(link);
+        songs = [
+            {
+                title: songInfo.title,
+                url: songInfo.video_url
+            }
+        ];
     }
 
     // If there's a server queue add the song
     let queue = serverQueues.get(message.guild.id);
     if (queue) {
-        // Add the song to the beginning of the queue and stop playing the current song
-        queue.songs.splice(1, 0, song)
+        // Update the queue to have the playing song at the beginning, the playlist of new songs, followed by the old queue
+        queue.songs = [queue.songs[0]].concat(songs, queue.songs.slice(1));
         queue.connection.dispatcher.end();
        
-        return;
+        return `Added ${songs.length} songs to the queue`;
     }
 
     // We need to create a new one!
-    await createNewQueue(message, song);
+    await createNewQueue(message, songs);
 
     // Play next song will pring messages for the bot
     playNextSong(message.guild.id, message.channel);
+
+    return `Added ${songs.length} songs to the queue`;
 }
 
 let enqueue = {};
@@ -144,22 +174,42 @@ enqueue.params = [
 enqueue.callback = async function(message, link) {
     verifyChannelPerms(message);
 
-    let songInfo = await ytdl.getInfo(link);
-    let song = {
-        title: songInfo.title,
-        url: songInfo.video_url
+    // Determine whether we are in a playlist
+    let songs = [];
+    if (link.indexOf('&list=') !== -1) {
+        // Query YouTube to get all the songs in the playlist
+        let result = await ytlist(link, ['name', 'url']);
+        result.data.playlist.forEach(obj => {
+            songs.push({
+                title: obj.name,
+                url: obj.url
+            });
+        })
+    } else {
+        // Grab the info for our one song we want to add
+        let songInfo = await ytdl.getInfo(link);
+        songs = [
+            {
+                title: songInfo.title,
+                url: songInfo.video_url
+            }
+        ];
     }
 
     // If there's a server queue add the song
     let queue = serverQueues.get(message.guild.id);
     if (queue) {
-        queue.songs.push(song);
+        queue.songs = queue.songs.concat(songs);
 
-        return `**${song.title}** has been added to the queue`;
+        if (songs.length === 1) {
+            return `**${songs[0].title}** has been added to the queue`;
+        }
+
+        return `**${songs.length}** songs have been added to the queue`;
     }
 
     // We need to create a new one!
-    await createNewQueue(message, song);
+    await createNewQueue(message, songs);
 
     // Play next song will pring messages for the bot
     playNextSong(message.guild.id, message.channel);
@@ -183,6 +233,7 @@ dequeue.callback = function(message) {
     // Current song is still at index 0, so remove index 1
     queue.songs.splice(1, 1);
 
+    // Info for the user
     let nextSong = 'Removed the next song in the queue\n';
     if (queue.songs.length > 1) {
         nextSong += `The next song is now **${queue.songs[1].title}**`;
@@ -202,6 +253,7 @@ skip.callback = function(message) {
 
     verifyChannelPerms(message);
 
+    // Go ahead and end the current stream. This will trigger moving to the next song in the queue.
     let queue = getServerQueue(guildId);
     queue.connection.dispatcher.end();
 }
@@ -215,6 +267,7 @@ stop.callback = function(message) {
 
     verifyChannelPerms(message);
 
+    // Empty the queue and end the stream, this will delete the queue
     let queue = getServerQueue(guildId);
     queue.songs = [];
     queue.connection.dispatcher.end();
@@ -231,6 +284,7 @@ clear.callback = function(message) {
 
     verifyChannelPerms(message);
 
+    // Empty the queue
     let queue = getServerQueue(guildId);
     queue.songs = [];
 
@@ -246,6 +300,7 @@ pause.callback = function(message) {
 
     verifyChannelPerms(message);
 
+    // Pause the stream
     let queue = getServerQueue(guildId);
     queue.connection.dispatcher.pause();
 
@@ -261,6 +316,7 @@ resume.callback = function(message) {
 
     verifyChannelPerms(message);
 
+    // Resume the stream if it's paused
     let queue = getServerQueue(guildId);
     if (queue.connection.dispatcher.paused) {
         queue.connection.dispatcher.resume();
@@ -279,22 +335,21 @@ getQueue.callback = function(message) {
     let guildId = message.guild.id;
 
     verifyChannelPerms(message);
-
+    
+    // Grab the queue
     let queue = getServerQueue(guildId);
-    let queueStr = 'First few songs of the queue are ';
+    let queueStr = '';
     if (queue.songs.length > 1) {
+        // Loop through the queue so long as we are below five and we have a song
         let i = 1;
-        while (i < 5) {
+        while (i < 5 && queue.songs[i]) {
             queueStr += `**${queue.songs[i].title}**, `
-
-            if (!queue.songs[i + 1]) {
-                break;
-            }
 
             i += 1;
         }
 
-        queueStr = queueStr.substr(0, queueStr.length - 2); 
+        // Costruct the string
+        queueStr = `There are ${queue.songs.length} songs in the queue. The first ${i} are ` + queueStr.substr(0, queueStr.length - 2); 
     } else {
         queueStr = 'There are no songs in the queue';
     }
