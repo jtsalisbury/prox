712b5aa60d4a5716ed0572bfbb701d34c7ef2fde
JT Salisbury <crazyscouter1@gmail.com>
2022-11-11 22:24:21 -0500

    update

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -8,7 +8,7 @@ help.executeViaIntegration = false;
 help.help = 'Takes you to the help page for Prox';
 help.category = 'Help';
 help.callback = async function() {
-    return 'Check out the help page at http://jtsalisbury.me:3001'
+    return 'Check out the help page at http://jtsalisbury.me:3001/help'
 }
 
 export let commands = [help];
